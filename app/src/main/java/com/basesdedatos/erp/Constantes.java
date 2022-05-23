213610151d2f6f0a48ad6f24492ea2cf3274c9db
LuisRocha800 <57692308+LuisRocha800@users.noreply.github.com>
2022-05-23 14:03:56 -0500

    Update Constantes.java

--- app/src/main/java/com/basesdedatos/erp/Constantes.java
+++ app/src/main/java/com/basesdedatos/erp/Constantes.java
@@ -2,7 +2,7 @@ package com.basesdedatos.erp;
 
 public class Constantes {
 
-    private final static String URL_WEB_SERVICE = "https://radiant-journey-98477.herokuapp.com/";
+    private final static String URL_WEB_SERVICE = "https://warm-refuge-72804.herokuapp.com/";
     public final static String URL_CLIENTE_X_ID = URL_WEB_SERVICE+"Clientes_Buscar_ID_GET.php";
     public final static String URL_CLIENTE_ULTIMA_COMPRA = URL_WEB_SERVICE+"Cliente_Ultima_Compra_POST.php";
     public final static String URL_CLIENTE_TOTAL_COMPRAS = URL_WEB_SERVICE+"Cliente_Total_Compras_POST.php";
