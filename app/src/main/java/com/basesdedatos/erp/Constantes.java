4859614a07faa4f417d6c5e1f9000aa5a85ac17d
LuisRocha800 <57692308+LuisRocha800@users.noreply.github.com>
2022-05-29 02:49:51 -0500

    Update Constantes.java

--- app/src/main/java/com/basesdedatos/erp/Constantes.java
+++ app/src/main/java/com/basesdedatos/erp/Constantes.java
@@ -2,7 +2,7 @@ package com.basesdedatos.erp;
 
 public class Constantes {
 
-    private final static String URL_WEB_SERVICE = "https://warm-refuge-72804.herokuapp.com/";
+    private final static String URL_WEB_SERVICE = "https://mysterious-reaches-30628.herokuapp.com/";
     public final static String URL_PERSONA_ID_WEB_SERVICE = URL_WEB_SERVICE+"Persona_Buscar_ID_GET.php";
     public final static String URL_USUARIOCLIENTE_ID_WEB_SERVICE = URL_WEB_SERVICE+"UsuarioCliente_Buscar_ID_GET.php";
     public final static String URL_USUARIOEMPLEADO_ID_WEB_SERVICE =URL_WEB_SERVICE+"UsuarioEmpleado_Buscar_ID_GET.php";
